7d36e0bc12a4a4223fa7eec7f6769929158f3a82
huy <leduchuy2801.com>
2018-09-07 16:36:42 +0700

    tinhsonguyento

---
+++ src/Kiemtrasonguyento/Songuyento.java
@@ -0,0 +1,35 @@
+package Kiemtrasonguyento;
+
+
+import java.util.Scanner;
+
+public class Songuyento {
+	  public static void main (String args[])
+	  {
+		  int n;
+		  Scanner sc=new Scanner (System.in);
+		  System.out.println ("nhap so n can kiem tra : ");
+		  n=sc.nextInt();
+		  if (n>0)
+		  {
+			  if(kiemtrasonguyento(n)==1)
+				  
+				  System.out.println(n+" la so nguyen to");
+			  else 
+				  System.out.println(n+" khong phai la so nguyen to");
+		  }
+	  }
+	  public static int kiemtrasonguyento(int n)
+	{
+		int dem=0 ;
+		for(int i=1;i<=n;i++)
+		{
+				if (n%i==0)
+					dem++;
+		}
+		if(dem==2)
+			return 1;
+		else return 0;
+		
+	  }
+}
